c0b4f9d65b65eb6470f86caf861e71552b610963
sudhakartippaluri-design <sudhakartippaluri@gmail.com>
2026-01-30 10:16:37 +0530

    Add files via upload

---
+++ Userinfo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication18.Models
+{
+    public class Userinfo
+    {
+
+        [Key]
+        public int UserId { get; set; }
+        public string Username { get; set; }
+                = string.Empty;
+
+
+        public string Password { get; set; }
+        = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string Phone { get; set; }
+        = string.Empty;
+        public DateTime Date { get; set; }
+    }
+}
+
+
